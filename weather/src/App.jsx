a2ad91ee32b3a6c304425a0caf02379aaf2a1ab5
Saurabh Dubey <saurabhdubey3712@gmail.com>
2024-10-03 20:02:06 +0530

    added

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -11,6 +11,14 @@ function App() {
         <span className='absolute top-4  right-58 text-5xl'>Weather</span>
         <input type='text' placeholder='Enter Your City' className='opacity-50  absolute top-24 left-20  text-wrap text-black placeholder-gray-500  border border-gray-300 p-2 rounded'></input>
         <button className='bg-red-800 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded absolute top-24 left-72'>Search</button>
+        <div>
+          <ul className='text-4xl absolute left-12 top-48'>
+            <li>Curent temperature :</li>
+            <li>Humidity :</li>
+            <li>Wind speed :</li>
+            <li>Weather condition :</li>
+          </ul>
+        </div>
       </div>
       </div> 
     </>
