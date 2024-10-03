11aa389b72f86fe2303b28b16216cd98c6e33efa
Saurabh Dubey <saurabhdubey3712@gmail.com>
2024-10-03 11:20:05 +0530

    update

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <>
-      <div className="bg-[url('/weatherbk.jpg')] h-screen w-full bg-cover bg-center"> abc</div> 
+      <div className="bg-[url('/weatherbk.jpg')] h-screen w-full bg-cover bg-center"> 
+      <div className="w-[60rem] h-[36rem] bg-black opacity-50 border-2 border-white p-5 text-white flex items-center justify-center absolute inset-0 m-auto"></div>
+      </div> 
     </>
   )
 }
